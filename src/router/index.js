9689c586633a82e69661c421e9084e7d825e002d
Manuel Colombini <manuelcolombini@gmail.com>
2023-10-26 23:52:22 -0300

    Adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,17 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/mySites',
+      name: 'MySites',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/MySitesView.vue')
+    },
+    {
+      path: '/addSite',
+      name: 'AddSite',
+      component: () => import('../views/AddSiteView.vue')
     }
   ]
 })
